25a61553c47cb6593c7ef1c72d57915aad66bdfe
William Wang <william.wang@toptal.com>
2017-09-16 01:07:31 +0800

    add base demo component

---
+++ demo/react/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import Survey from '../../src';
+import styles from './styles';
+
+export default class demo extends React.Component {
+  render() {
+    return (
+      <Survey />
+    );
+  }
+}
