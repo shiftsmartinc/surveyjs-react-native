b63f4406b22fd851b888715fcca16d21e6851bc5
William Wang <william.wang@toptal.com>
2017-09-26 16:28:22 +0800

    add keyboardType for QuestionText

--- lib/QuestionText.tsx
+++ lib/QuestionText.tsx
@@ -1,19 +1,37 @@
 import * as React from 'react';
-import { TextInput } from 'react-native';
+import { TextInput, KeyboardType } from 'react-native';
 
 import styles, { LINE_HEIGHT } from './styles/questionText';
 
 interface Props {
   placeholder?: string;
+  inputType?: string;
   multiline?: boolean;
   rows?: number;
   value: string;
   onChange: () => {};
 }
 
-export default class QuestionText extends React.Component<Props, any>{
+export default class QuestionText extends React.Component<Props, any> {
+  getKeyboardType = ():KeyboardType => {
+    const { inputType = 'text' } = this.props;
+    let keyboardType = 'default';
+    switch(inputType) {
+      case 'number':
+        keyboardType = 'numeric';
+        break;
+      case 'email':
+        keyboardType = 'email-address';
+        break;
+      default:
+    }
+    return keyboardType as KeyboardType;
+  }
+
   render() {
     const { rows = 1 } = this.props;
+    const keyboardType = this.getKeyboardType();
+    // const keyboardType = keyboardTypeEnums[this.props.inputType || 'text'];
     return (
       <TextInput
         style={[
@@ -26,6 +44,7 @@ export default class QuestionText extends React.Component<Props, any>{
         numberOfLines={rows}
         underlineColorAndroid={'transparent'}
         blurOnSubmit={!this.props.multiline}
+        keyboardType={keyboardType}
       />
     );
   }
