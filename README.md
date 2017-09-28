d654c6cb58390d498da9341bad7092ef0dc45d76
William Wang <william.wang@toptal.com>
2017-09-28 14:48:14 +0800

    add react-native-image-crop-picker
    replace react-native-image-picker with react-native-image-picker
    since the previous `react-native-image-picker` does not support choose
    both images and video in android device.

--- README.md
+++ README.md
@@ -4,4 +4,11 @@
 
 Please follow the installtion steps of the native-plugins:
 
-* [react-native-image-picker](https://github.com/react-community/react-native-image-picker)
+* [react-native-image-crop-picker](https://github.com/ivpusic/react-native-image-crop-picker)
+
+
+### run example
+1. ./: npm install
+2. ./: npm run link
+3. ./ios: `pod install`
+4. ./: `npm run ios-example` or `npm run android-example`
--- lib/QuestionFile.tsx
+++ lib/QuestionFile.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import { View, Text, Image, Alert } from 'react-native';
 import TouchableWithFeedback from './TouchableWithFeedback';
-import ImagePicker from 'react-native-image-picker';
+import ImagePicker, { Image as ImageInterface } from 'react-native-image-crop-picker';
+
 
 import styles from './styles/questionFile';
 
@@ -15,43 +16,29 @@ interface Props {
   onChange(value, comment?);
 }
 
-const pickerOptions = {
-  title: 'Select Your File',
-};
-
 export default class QuestionHtml extends React.Component<Props, any> {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      sourceUri: null,
-    };
-  }
-
   openPicker = () => {
-    ImagePicker.showImagePicker(pickerOptions, (response) => {
-      if (response.error) {
-        return;
-      }
+    ImagePicker.openPicker({})
+      .then((image) => {
+        const response = image as ImageInterface;
+        if (this.props.maxSize && response.size > this.props.maxSize) {
+          Alert.alert(
+            'FileSize',
+            'Too Large FileSize',
+            [
+              { text: 'OK', onPress: () => { } },
+            ],
+            { cancelable: true }
+          );
+          return;
+        }
+        const value = this.props.storeDataAsText ?
+          `data:${response.mime};base64,${response.data}` : response.path;
+        this.props.onChange(value);
+      }).catch(() => {
+      });
 
-      if (this.props.maxSize && response.fileSize > this.props.maxSize) {
-        Alert.alert(
-          'FileSize',
-          'Too Large FileSize',
-          [
-            { text: 'OK', onPress: () => {} },
-          ],
-          { cancelable: true }
-        );
-        return;
-      }
-      const value = this.props.storeDataAsText ?
-        `data:image/jpeg;base64,${response.data}` : response.uri;
-      this.props.onChange(value);
-      this.setState({
-        sourceUri: response.uri,
-      })
-    });
   }
   render() {
     const {
