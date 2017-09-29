ed63fb12c710217d245922e779c33beb0347690f
William Wang <william.wang@toptal.com>
2017-09-29 11:09:47 +0800

    fix html height in ios
    can't fingre out why the height is unstable, add a setTimeout to it to
    walk around.

--- lib/QuestionHtml.tsx
+++ lib/QuestionHtml.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
 
-import AutoHeightWebView from 'react-native-webview-autoheight';
-
+import {
+  Dimensions,
+  WebView,
+} from 'react-native';
 
 interface Props {
   html: string;
@@ -17,13 +19,82 @@ const injectedStyles = `
   </style>
 `;
 
+
+const injectedScript = `(function () {
+  function getAndSendHeight() {
+    let height = 0;
+    if (document.documentElement.clientHeight > document.body.clientHeight) {
+      height = document.documentElement.clientHeight
+    }
+    else {
+      height = document.body.clientHeight
+    }
+
+    window.postMessage(height);
+  }
+  function waitForBridge() {
+    if (window.postMessage.length !== 1) {
+      setTimeout(waitForBridge, 200);
+    }
+    else {
+      getAndSendHeight();
+    }
+  }
+  setTimeout(waitForBridge, 50);
+})();`;
+
+class MyWebView extends React.Component<any, any> {
+  state = {
+    webViewHeight: Number
+  };
+
+  static defaultProps = {
+    autoHeight: true,
+  }
+
+  constructor(props: Object) {
+    super(props);
+    this.state = {
+      webViewHeight: this.props.defaultHeight
+    }
+
+    this._onMessage = this._onMessage.bind(this);
+  }
+
+  _onMessage(e) {
+    console.log('onMessage: ', e.nativeEvent.data);
+    this.setState({
+      webViewHeight: parseInt(e.nativeEvent.data)
+    });
+  }
+
+  render() {
+    const _w = this.props.width || Dimensions.get('window').width;
+    const _h = this.props.autoHeight ? this.state.webViewHeight : this.props.defaultHeight;
+    return (
+      <WebView
+        injectedJavaScript={injectedScript}
+        scrollEnabled={this.props.scrollEnabled || false}
+        onMessage={this._onMessage}
+        javaScriptEnabled={true}
+        automaticallyAdjustContentInsets={true}
+        {...this.props}
+        style={[{ width: _w }, this.props.style, { height: _h }]}
+      />
+    )
+  }
+}
+
+
 export default class QuestionHtml extends React.Component<Props, any> {
   render() {
     return (
-      <AutoHeightWebView
+      <MyWebView
         source={{
           html: `${injectedStyles}${this.props.html}`,
         }}
+        defaultHeight={1}
+        startInLoadingState={true}
         scrollEnabled={false}
         scalesPageToFit={true}
       />
