101feeee4fc18cd1a4747f9f95a7b1fc597cd17c
William Wang <william.wang@toptal.com>
2017-10-31 17:07:47 +0800

    add mobx-logger

--- example/react/index.js
+++ example/react/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import { Alert } from 'react-native';
 import Survey from 'surveyjs-react-native';
+import { enableLogging } from 'mobx-logger';
 import json from './survey.json';
+
+enableLogging({
+  predicate: () => __DEV__ && Boolean(global.navigator.userAgent),
+  action: true,
+  reaction: false,
+  transaction: false,
+  compute: false,
+});
 
 export default class Example extends React.Component {
   onComplete = (results) => {
